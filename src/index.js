32aa09f3e643385cd1411e163479d514c8222df1
ixphinx <48696004+ixphinx@users.noreply.github.com>
2019-11-12 23:28:04 -0300

    correccion

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 
-//Configuracion hola mi amor
+//Configuracion
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'ejs');
