578067d18cc7c136266f5fd022de780e79022d98
ixphinx <48696004+ixphinx@users.noreply.github.com>
2019-11-12 23:15:13 -0300

    Agregado de process.env.PORT ||

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 
 //Configuracion
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'ejs');
 
